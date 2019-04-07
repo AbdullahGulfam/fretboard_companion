31a102500947c7faded1ad393c8a3fa2cb907972
Abdullah Gulfam <abdullahgulfam1997@gmail.com>
2019-04-07 00:40:15 -0400

    Menu Update
    - Created Menu.js and Menu.css

---
+++ fretboard_companion/src/Menu.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./CSS/Menu.css";
+
+export class Menu extends Component {
+  render() {
+    return (
+      <div className="main">
+        <h1 className="title">Fretboard Companion</h1>
+      </div>
+    );
+  }
+}
+
+export default Menu;
